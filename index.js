01bfb5c080121e2604e0c071c39f50491c1035e7
Abdullah13130 <96126010+Abdullah13130@users.noreply.github.com>
2023-10-20 11:01:15 +0600

    mongodb add

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zvnp5zo.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollection = client.db('productDB').collection('products');
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req,res)=>{
+    res.send('server site running in local with nodemon')
+})
+
+app.listen(port, ()=>{
+    console.log(`server site port ${port}`);
+})
+
+
+// https://preview.themeforest.net/item/leo-digital-prestashop-theme/full_screen_preview/6677692?_ga=2.170602083.234748444.1697606827-1675548590.1696611455
+
+// http://preview.themeforest.net/item/applauz-startup-wordpress-theme/full_screen_preview/21053363?_ga=2.131647692.234748444.1697606827-1675548590.1696611455
